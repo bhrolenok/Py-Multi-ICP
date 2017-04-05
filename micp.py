a847c852a4af9dc4a803fdde42522e0332a4fb43
Brian Hrolenok <hroly@kawauso.local>
2017-04-05 18:33:42 -0400

    Initial commit
    The bare bones code snippets for matching a model to the first
    detections found in the frame. Currently doesn't iterate/refine
    initial fit, doesn't detect multiple objects, and does not track
    accross frames.

---
+++ micp.py
@@ -0,0 +1,59 @@
+# micp.py
+import numpy, scipy.optimize
+import pyqtgraph
+import skvideo.io
+import skimage.io, skimage.draw
+
+def make_tform_mat(tform):
+	return numpy.matrix([[numpy.cos(tform[2]), -numpy.sin(tform[2]), tform[0]],\
+		                 [numpy.sin(tform[2]),  numpy.cos(tform[2]), tform[1]],\
+		                 [                  0,                    0,        1]])
+
+def tform_pts(pts,tform):
+	tform_mat = make_tform_mat(tform)
+	aug_pts = numpy.column_stack([pts,numpy.ones(shape=(len(pts),1))])
+	return tform_mat.dot(aug_pts.T).T[:,:-1]
+
+def tform_error(tform,src,dst):
+	tform_mat = make_tform_mat(tform)
+	augmented_src = numpy.column_stack([src,numpy.ones(shape=(len(src),1))])
+	tformed_src = tform_mat.dot(augmented_src.T).T[:,:-1]
+	return numpy.linalg.norm(tformed_src-dst,axis=1).sum()
+
+def est_tform(detection_pts,model_pts):
+	shifted_model_pts = model_pts+detection_pts[0]-model_pts[0]
+	closest_detection_idx,residuals = pyflann.FLANN().nn(detection_pts.astype('float'),shifted_model_pts.astype('float'))
+	closest_detection_pts = detection_pts[closest_detection_idx]
+	opt_res = scipy.optimize.minimize(fun=tform_error,\
+		                              x0=numpy.array([0,0,0]),\
+		                              args=(model_pts,closest_detection_pts))
+	return opt_res
+
+def mask_to_pts(mask):
+	row_idxs, col_idxs = numpy.mgrid[0:mask.shape[0],0:mask.shape[1]]
+	return numpy.array(zip(row_idxs[mask],col_idxs[mask]))
+
+def bbox_detect(model,frame,bg_img,copy_frame=True):
+	if copy_frame:
+		frame = frame.copy()
+	model_mask = model[:,:,3]>200
+	model_pts = mask_to_pts(model_mask)
+	bg_subbed = numpy.abs(frame-bg_img)
+	if len(bg_subbed.shape)==3:
+		bg_subbed = bg_subbed.sum(axis=-1)
+	bg_mask = bg_subbed>(bg_subbed.max()/2.0)
+	detection_pts = mask_to_pts(bg_mask)
+	opt_res = est_tform(detection_pts,model_pts)
+	tform = opt_res.x
+	bbox_pts = numpy.array([[0,0],[0,model.shape[1]], [model.shape[0],model.shape[1]], [model.shape[0],0]])
+	tformed_bbox_pts = tform_pts(bbox_pts,tform).round().astype('int')
+	rr,cc = skimage.draw.polygon_perimeter(tformed_bbox_pts[:,0].flatten(),tformed_bbox_pts[:,1].flatten(),shape=frame.shape,clip=True)
+	frame[rr,cc,:]=[255,0,0]
+	return frame
+
+def bgsub_snippets():
+	vd = skvideo.io.vread("Downloads/2011MARS_Aphaenogaster_Fruitfly_PredPrey_mar28/MVI_9582.MOV",num_frames=100)
+	img_med = numpy.median(vd,axis=0)
+	bg_subbed = numpy.abs(vd-img_med)
+	pyqtgraph.image(bg_subbed)
+	# skvideo.io.vwrite("Downloads/2011MARS_Aphaenogaster_Fruitfly_PredPrey_mar28/detections.mp4",bg_subbed)
